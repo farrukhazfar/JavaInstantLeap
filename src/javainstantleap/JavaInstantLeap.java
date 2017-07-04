25cd277330ea27c3fadb48047adee930960d5fca
Farrukh Azfar <farrukh.azfar@physics.ox.ac.uk>
2017-07-04 01:31:33 +0100

    formatting / comments

--- src/javainstantleap/JavaInstantLeap.java
+++ src/javainstantleap/JavaInstantLeap.java
@@ -1,19 +1,21 @@
 package javainstantleap;
+
 import java.net.*;
 import java.io.*;
 import java.lang.Object;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  *
  * @author Farrukh Azfar
  */
-
-
-    
-
 public class JavaInstantLeap {
 
     /**
@@ -21,69 +23,72 @@ public class JavaInstantLeap {
      */
     public static void main(String[] args) throws MalformedURLException, IOException {
 
-	URL oracle = new URL("https://www.ietf.org/timezones/data/leap-seconds.list");
-	BufferedReader in = new BufferedReader(
-	new InputStreamReader(oracle.openStream()));
+        URL oracle = new URL("https://www.ietf.org/timezones/data/leap-seconds.list");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
 
-	String inputLine;
-	String lastLine = "";
-	String testLine = ""; // always check this line to see that its 36
-	int ii = 0;
-	while ((inputLine = in.readLine()) != null){
-	    if(inputLine.trim().startsWith("#")){
-		    //do nothing
+        List<String> stringList = new ArrayList<String>();
 
-	    }
-		else{
-		    System.out.println(inputLine);
-		    lastLine = inputLine;
-		    ii++;
-		    if(ii==27) {
-			testLine = inputLine;
-		    }
-		}
+        String inputLine;
+        String lastLine = "";
+        String seconLast = "";
+        int ii = 0;
 
-	}
-	
-	String [] array1 = lastLine.split("#");
-        String [] testArray1 = testLine.split("#");
+        while ((inputLine = in.readLine()) != null) {
+            if (inputLine.trim().startsWith("#")) {
+                //do nothing
 
-	// you'll get the first piece of the row in the file on the web ...
+            } else {
+                System.out.println(inputLine);
+                lastLine = inputLine;
+                stringList.add(inputLine);
+
+            }
+
+        }
+
+        String[] breakLast = stringList.get(stringList.size() - 1).split("#");
+        String[] testString = stringList.get(27).split("#");
+
+        // you'll get the first piece of the row in the file on the web ...
         System.out.println("\n");
-	System.out.println("Epoch time from 1900 and leap second before array is split again : ");
-	System.out.println(array1[0]);
-	System.out.println(array1[1]);
+        System.out.println("Epoch time from 1900 and leap second before array is split again : ");
+        System.out.println(breakLast[0]);
+        System.out.println();
 
-	// epoch time in seconds from 1900 (IMPORTANT !!!!!) 
-	/// array2 pertains to the lastLine and testarray to the testLine
-	String [] array2 = array1[0].split("\\s+");
+        // epoch time in seconds from 1900 (IMPORTANT !!!!!) 
+        /// array2 pertains to the lastLine and testarray to the testLine
+        String[] breakLastAgain = breakLast[0].split("\\s+");
 
-	Long epochUNIX =0L;
-	Long epochUNIXTest = 0L;
-	Long offset = 2208988800L;
-	epochUNIX = epochUNIX.valueOf(array2[0])-offset;
-         System.out.println("\n");
-	System.out.println("The epoch time from 1970 (Unix Epoch Time) on January 1 2017:");
-	System.out.println(epochUNIX);
-
-	String [] testArray2 = testArray1[0].split("\\s+");
-	epochUNIXTest = epochUNIXTest.valueOf(testArray2[0])-offset;
+        Long epochUNIX = 0L;
+        Long epochUNIXTest = 0L;
+        Long offset = 2208988800L;
+        epochUNIX = epochUNIX.valueOf(breakLastAgain[0]) - offset;
         System.out.println("\n");
-	System.out.println("The epoch time from 1970 (Unix Epoch Time) on July 1 2015:");
-	System.out.println(epochUNIXTest);
+        System.out.println("The epoch time from 1970 (Unix Epoch Time) on January 1 2017:");
+        System.out.println(epochUNIX);
 
-     // now play with the instant class - 
-     
-     Instant instant = Instant.now();
-     
-     System.out.println("Print out Epoch seconds of this instant");
-     System.out.println(instant.getEpochSecond());
+        String[] brokenTestString = testString[0].split("\\s+");
+        epochUNIXTest = epochUNIXTest.valueOf(brokenTestString[0]) - offset;
+        System.out.println("\n");
+        System.out.println("The epoch time from 1970 (Unix Epoch Time) on July 1 2015 array element 27 should be 36:");
+        System.out.println(epochUNIXTest);
 
-     // need to add nano seconds - need to check if 1 Jul 2015 epoch is correct - 
-	in.close();
-         
+        // now play with the instant class - 
+        Instant instant = Instant.now();
+        System.out.println("\n");
+
+        System.out.println("Print out Epoch seconds of this instant");
+        System.out.println(instant.getEpochSecond());
+        System.out.println("\n");
+
+        System.out.println("Second Last and Last entries - these have not been split yet");
+
+        System.out.println(stringList.get(stringList.size() - 2));
+        System.out.println(stringList.get(stringList.size() - 1));
+
+        // need to add nano seconds - need to check if 1 Jul 2015 epoch is correct - 
+        in.close();
+
     }
 }
-    
-        
-   
